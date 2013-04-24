51466dfa40bc3820d0385d5b832f91f1837dcb33
git <sebastianclaici@gmail.com>
2013-04-24 20:08:35 +0100

    FMT: Added comments to the C++ implementation of a disjoint set

--- cpp/data_structures/disjoint_set/disjoint_set.cpp
+++ cpp/data_structures/disjoint_set/disjoint_set.cpp
@@ -2,38 +2,76 @@
 #include <iostream>
 #include <vector>
 
+// Implementation of a disjoint_set data structure.
+//
+// A disjoint_set data structure is a data structure that keeps track of a
+// set of elements partitioned into a number of disjoint (nonoverlapping)
+// subsets. A union-find algorithm is an algorithm that performs two
+// operations on such a data structure:
+//      * Find: determine which subset a particular element is in.
+//      * Union: Join two subsets into a single subset.
+//
+// For more information: 
+// http://en.wikipedia.org/wiki/Disjoint-set_data_structure
 class disjoint_set {
 public:
     explicit disjoint_set(int N);
+    ~disjoint_set();
 
     int find(int val);
     void unite(int lhs, int rhs);
     bool same_component(int lhs, int rhs);
 
 private:
-    std::vector<int> parent;
-    std::vector<int> rank;
+    int *parent;
+    int *rank;
 };
 
-disjoint_set::disjoint_set(int N) {
-    rank.resize(N);
-    parent.resize(N);
+// Constructs a disjoint-set of specified size.
+//
+// Args:
+//      size: size of the resulting data structure
+disjoint_set::disjoint_set(int size) {
+    parent = new int[size];
+    rank = new int[size];
 
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < size; ++i)
         parent[i] = i;
 }
 
+disjoint_set::~disjoint_set() {
+    delete[] parent;
+    delete[] rank;
+}
+
+// Find the component in which val is located.
+//
+// Args:
+//      val: the element for which we want to find the enclosing component
+//
+// Returns: the root element of the component containing val
 int disjoint_set::find(int val) {
     if (val == parent[val])
         return val;
 
+    // To speed-up future queries, path compression is applied. We only
+    // care about the path between an element and the root of its component
+    // so intermediate paths can be compressed.
     return parent[val] = find(parent[val]);
 }
 
+// Unite the components which contain lhs and rhs.
+//
+// Args:
+//      lhs: element of component to unite with component of rhs
+//      rhs: elemenf of component to unite with component of lhs
 void disjoint_set::unite(int lhs, int rhs) {
     int parent_lhs = find(lhs);
     int parent_rhs = find(rhs);
 
+    // To avoid having an unbalanced tree, a union pairs the lower
+    // weighted sub-tree as the child of the heavier sub-tree.
+    // If both elements are equally weighted, one is chosen at random.
     if (rank[parent_lhs] < rank[parent_rhs])
         parent[parent_lhs] = parent_rhs;
     else
@@ -43,6 +81,14 @@ void disjoint_set::unite(int lhs, int rhs) {
         ++rank[parent_lhs];
 }
 
+// Query whether lhs and rhs are in the same component.
+//
+// Args:
+//      lhs: component to query
+//      rhs: component to query
+//
+// Returns: true if lhs and rhs are in the same component,
+//      false otherwise
 bool disjoint_set::same_component(int lhs, int rhs) {
     return find(lhs) == find(rhs);
 }
