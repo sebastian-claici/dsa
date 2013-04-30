fc465b36de0e18f26318785e81420b824d2676fa
git <sebastianclaici@gmail.com>
2013-04-30 19:19:39 +0100

    ADD: Comments to the topological sort implementations

--- cpp/algorithms/topological_sort/topological_sort.cpp
+++ cpp/algorithms/topological_sort/topological_sort.cpp
@@ -5,22 +5,53 @@
 #include <unordered_set>
 #include <vector>
 
+// Implemenation of a graph data structure. This example only
+// creates directed, unweighted graphs. Each graph is represented by 
+// a set of nodes, and an adjacency list for each node.
+//
+// This is a simple implementation just for the sake of the topological
+// sort algorithm.
+//
+// Note: T must define a hash value
 template <class T>
 class graph {
 public:
+    // Creates an empty graph with no nodes and no edges.
     explicit graph() {}
 
+    // Creates a graph with nodes taken from the range [begin, end).
+    //
+    // Args:
+    //      begin: iterator pointing to the beginning of a range
+    //      end: iterator pointing to the end of a range
     template <class InputIterator>
     explicit graph(InputIterator begin, InputIterator end) 
         : nodes(begin, end) {}
 
+    // Adds an edge between the nodes src and dst. Adds src and dst to 
+    // the list of nodes.
+    //
+    // Args:
+    //      src: start node of the edge
+    //      dst: end node of the edge
     void add_edge(T src, T dst) {
         nodes.insert(src);
         nodes.insert(dst);
         edges[src].push_back(dst);
     }
 
+    // Retrieve a topological sort of this graph.
+    //
+    // A topological sort of a directed graph is a linear ordering of its
+    // vertices such that for every directed edge u-v from node u to node 
+    // v, u comes before v in the ordering.
+    //
+    // For more information: http://en.wikipedia.org/wiki/Topological_sorting
+    //
+    // Returns: a vector of nodes such that if u-v is an edge in the graph,
+    //      then u comes before v in the vector.
     std::vector<T> topological_sort() {
+        // in_degree represents the amount of edges coming into each node
         std::unordered_map<T, int> in_degree;
         for (auto entry : edges) {
            for (auto neighbour : entry.second) {
@@ -33,6 +64,9 @@ public:
             if (in_degree[node] == 0)
                 q.push(node);
 
+        // Gradually remove nodes that have no incoming edges; remove all
+        // edges starting from the removed node, and add all nodes that 
+        // have in-degree 0 to the queue.
         std::vector<T> result;
         while (!q.empty()) {
             T node = q.front();
@@ -57,6 +91,7 @@ private:
      std::unordered_set<T> nodes;
 };
 
+// Test code
 int main()
 {
     std::ifstream fin("sortaret.in");
--- java/src/algorithms/topological_sort/TopologicalSort.java
+++ java/src/algorithms/topological_sort/TopologicalSort.java
@@ -4,6 +4,13 @@ import java.io.*;
 import java.util.*;
 
 /**
+ * Implementation of a graph data structure. This example only
+ * creates directed, unweighted graphs. Each graph is represented
+ * by a set of nodes, and an adjacency list for each node.
+ *
+ * This is a simple implementation just for the sake of the topological
+ * sort algorithm.
+ *
  * @author Sebastian Claici
  */
 public class TopologicalSort {
@@ -13,16 +20,30 @@ public class TopologicalSort {
         private final Set<T> nodes;
         private final Map<T, List<T>> edges;
 
+        /**
+         * Creates an empty graph with no nodes and no edges.
+         */
         public Graph() {
             nodes = new HashSet<T>();
             edges = new HashMap<T, List<T>>();
         }
 
+        /**
+         * Creates a graph with nodes taken from the nodes collection.
+         *
+         * @param nodes Initial set of nodes for this graph.
+         */
         public Graph(Collection<T> nodes) {
             this.nodes = new HashSet<T>(nodes);
             this.edges = new HashMap<T, List<T>>();
         }
 
+        /**
+         * Adds an edge between src and dst.
+         *
+         * @param src Start node of the edge.
+         * @param dst End node of the edge.
+         */
         public void addEdge(T src, T dst) {
             nodes.add(src);
             nodes.add(dst);
@@ -35,22 +56,40 @@ public class TopologicalSort {
             }
         }
 
+        /**
+         * Retrieve a topological sort of this graph.
+         *
+         * A topological sort of a directed graph is a linear ordering of its
+         * vertices such that for every directed edge u-v from node u to node v,
+         * u comes before v in the ordering.
+         *
+         * For more information: http://en.wikipedia.org/wiki/Topological_sorting
+         *
+         * @return A list of nodes such that if u-v is an edge in the graph,
+         *      then u comes before v in the list.
+         */
         public List<T> topologicalSort() {
-            Map<T, Integer> in_degree = new HashMap<T, Integer>();
+            // inDegree represents the amount of edges coming into each node
+            Map<T, Integer> inDegree = new HashMap<T, Integer>();
             for (T node : nodes)
-                in_degree.put(node, 0);
+                inDegree.put(node, 0);
 
             for (T node : edges.keySet()) {
                 for (T neighbour : edges.get(node)) {
-                    in_degree.put(neighbour, in_degree.get(neighbour) + 1);
+                    inDegree.put(neighbour, inDegree.get(neighbour) + 1);
                 }
             }
 
             Queue<T> queue = new LinkedList<T>();
             for (T node : nodes)
-                if (in_degree.get(node) == 0)
+                if (inDegree.get(node) == 0)
                     queue.add(node);
 
+            /*
+             * Gradually remove nodes that have no incoming edges; remove all edges
+             * starting in the removed node, and add all nodes that now have in-degree
+             * 0 to the queue.
+             */
             List<T> result = new ArrayList<T>();
             while (!queue.isEmpty()) {
                 T node = queue.poll();
@@ -60,11 +99,11 @@ public class TopologicalSort {
                     continue;
 
                 for (T neighbour : edges.get(node)) {
-                    int degree = in_degree.get(neighbour);
+                    int degree = inDegree.get(neighbour);
                     --degree;
                     if (degree == 0)
                         queue.add(neighbour);
-                    in_degree.put(neighbour, degree);
+                    inDegree.put(neighbour, degree);
                 }
             }
 
@@ -72,6 +111,7 @@ public class TopologicalSort {
         }
     }
 
+    // Test code
     public static void main(String[] args) throws IOException {
         BufferedReader buf = new BufferedReader(new FileReader("topsort.in"));
         StringTokenizer st = new StringTokenizer(buf.readLine());
